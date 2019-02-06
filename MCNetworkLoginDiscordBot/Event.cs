077a9ce565745d2895643204af9efbb443e424a4
Łukasz Jurczyk <zabszk@protonmail.ch>
2019-02-07 00:19:38 +0100

    Fixes

--- MCNetworkLoginDiscordBot/Event.cs
+++ MCNetworkLoginDiscordBot/Event.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
+using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
 
 namespace MCNetworkLoginDiscordBot
@@ -20,92 +22,119 @@ namespace MCNetworkLoginDiscordBot
 
 		internal static Task MessageReceived(MessageCreateEventArgs e)
 		{
-			if (!e.Message.Content.StartsWith(Program.Config.CommandPrefix)) return Task.CompletedTask;
-			var message = e.Message.Content.Substring(Program.Config.CommandPrefix.Length).Split(' ');
-			switch (message[0].ToLower())
-			{
-				case "register":
-					if (e.Channel.IsPrivate || e.Channel.Id != Program.Config.RegisterChannel)
-					{
-						e.Guild.GetMemberAsync(e.Author.Id).Result.SendMessageAsync(
-							$"Command \"register\" can only be executed on channel <#{Program.Config.RegisterChannel}>.");
+			ProcessMessage(e).GetAwaiter().GetResult();
+			return Task.CompletedTask;
+		}
 
-						return Task.CompletedTask;
-					}
-					else
-					{
+		private static async Task ProcessMessage(MessageCreateEventArgs e)
+		{
+			try
+			{
+				if (!e.Message.Content.StartsWith(Program.Config.CommandPrefix)) return;
+				var message = e.Message.Content.Substring(Program.Config.CommandPrefix.Length).Split(' ');
+
+				DiscordMember sender = null;
+
+				if (!e.Channel.IsPrivate)
+				{
+					var senderTask = e.Guild.GetMemberAsync(e.Author.Id);
+					await senderTask;
+					sender = senderTask.Result;
+				}
+
+				switch (message[0].ToLower())
+				{
+					case "register":
+						if (sender == null || e.Channel.IsPrivate || e.Channel.Id != Program.Config.RegisterChannel)
+						{
+							if (sender != null)
+								await sender.SendMessageAsync(
+								$"Command \"register\" can only be executed on channel <#{Program.Config.RegisterChannel}>.");
+
+							else
+							await e.Channel.SendMessageAsync($"Command \"register\" can only be executed on channel <#{Program.Config.RegisterChannel}>.");
+
+							return;
+						}
+
 						if (message.Length != 2)
 						{
-							e.Guild.GetMemberAsync(e.Author.Id).Result.SendMessageAsync(
+							await e.Guild.GetMemberAsync(e.Author.Id).Result.SendMessageAsync(
 								$"Correct syntax is register YourMinecraftUsername");
 
-							return Task.CompletedTask;
+							return;
 						}
 
 						if (message[1].Length < 4 || message[1].Length > 16)
 						{
-							e.Guild.GetMemberAsync(e.Author.Id).Result.SendMessageAsync(
+							await e.Guild.GetMemberAsync(e.Author.Id).Result.SendMessageAsync(
 								$"Username must be between 4 and 16 characters.");
 
-							return Task.CompletedTask;
+							return;
 						}
 
-						var result = HttpQuery.Post(Program.Config.ApiUrl, $"action=register&token={Program.Config.ApiUrl}&DiscordID={e.Author.Id}&username={Base64.Base64Encode(message[1])}");
-						e.Guild.GetMemberAsync(e.Author.Id).Result.SendMessageAsync(
-							$"Request processed, result: " + result + $". To change your password use **IN DIRECT MESSAGE** (!) : {Program.Config.CommandPrefix}passwd YourNewPassword");
-					}
-					break;
+						var result = HttpQuery.Post(Program.Config.ApiUrl,
+							$"action=register&token={Program.Config.ApiUrl}&DiscordID={e.Author.Id}&username={Base64.Base64Encode(message[1])}");
+						await e.Guild.GetMemberAsync(e.Author.Id).Result.SendMessageAsync(
+							$"Request processed, result: " + result +
+							$". To change your password use **IN DIRECT MESSAGE** (!) : {Program.Config.CommandPrefix}passwd YourNewPassword");
+						break;
 
-				case "unregister":
-					if (e.Channel.IsPrivate || e.Channel.Id != Program.Config.RegisterChannel)
-					{
-						e.Guild.GetMemberAsync(e.Author.Id).Result.SendMessageAsync(
-							$"Command \"unregister\" can only be executed on channel <#{Program.Config.RegisterChannel}>.");
+					case "unregister":
+						if (sender == null || e.Channel.IsPrivate || e.Channel.Id != Program.Config.RegisterChannel)
+						{
+							if (sender != null)
+								await sender.SendMessageAsync(
+									$"Command \"unregister\" can only be executed on channel <#{Program.Config.RegisterChannel}>.");
 
-						return Task.CompletedTask;
-					}
-					else
-					{
-						var result = HttpQuery.Post(Program.Config.ApiUrl, $"action=unregister&token={Program.Config.ApiUrl}&DiscordID={e.Author.Id}");
-						e.Guild.GetMemberAsync(e.Author.Id).Result.SendMessageAsync(
-							$"Request processed, result: " + result);
-					}
-					break;
+							else
+								await e.Channel.SendMessageAsync($"Command \"unregister\" can only be executed on channel <#{Program.Config.RegisterChannel}>.");
 
-				case "passwd":
-					if (!e.Channel.IsPrivate)
-					{
-						e.Guild.GetMemberAsync(e.Author.Id).Result.SendMessageAsync(
-							$"Command \"passwd\" can only be executed in direct message.");
+							return;
+						}
 
-						return Task.CompletedTask;
-					}
-					else
-					{
+						var result2 = HttpQuery.Post(Program.Config.ApiUrl,
+							$"action=unregister&token={Program.Config.ApiUrl}&DiscordID={e.Author.Id}");
+						await e.Guild.GetMemberAsync(e.Author.Id).Result.SendMessageAsync(
+							$"Request processed, result: " + result2);
+						break;
+
+					case "passwd":
+						if (!e.Channel.IsPrivate)
+						{
+							await sender.SendMessageAsync(
+								$"Command \"passwd\" can only be executed in direct message.");
+
+							return;
+						}
+
 						if (message.Length != 2)
 						{
-							e.Guild.GetMemberAsync(e.Author.Id).Result.SendMessageAsync(
+							await e.Channel.SendMessageAsync(
 								$"Correct syntax is passwd YourNewPassword");
 
-							return Task.CompletedTask;
+							return;
 						}
 
 						if (message[1].Length < 8)
 						{
-							e.Guild.GetMemberAsync(e.Author.Id).Result.SendMessageAsync(
+							await e.Channel.SendMessageAsync(
 								$"Password must have at least 8 characters.");
 
-							return Task.CompletedTask;
+							return;
 						}
 
-						var result = HttpQuery.Post(Program.Config.ApiUrl, $"action=passwd&token={Program.Config.ApiUrl}&DiscordID={e.Author.Id}&password={Base64.Base64Encode(message[1])}");
-						e.Guild.GetMemberAsync(e.Author.Id).Result.SendMessageAsync(
-							$"Request processed, result: " + result);
-					}
-					break;
+						var result3 = HttpQuery.Post(Program.Config.ApiUrl,
+							$"action=passwd&token={Program.Config.ApiUrl}&DiscordID={e.Author.Id}&password={Base64.Base64Encode(message[1])}");
+						await e.Channel.SendMessageAsync(
+							$"Request processed, result: " + result3);
+						break;
+				}
 			}
-
-			return Task.CompletedTask;
+			catch (Exception ex)
+			{
+				Console.WriteLine("[EXCEPTION] " + ex.GetType() + ": " + ex.Message + "\r\n" + ex.StackTrace);
+			}
 		}
 	}
 }
